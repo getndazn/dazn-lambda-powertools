32f6c23256685fa15981dfbede98b701fe284cfc
Yan Cui <theburningmonk@gmail.com>
2018-06-20 19:42:53 +0200

    - added pattern for step functions

---
+++ packages/lambda-powertools-pattern-step-functions/index.js
@@ -0,0 +1,20 @@
+const middy = require('middy')
+const sampleLogging = require('@perform/lambda-powertools-middleware-sample-logging')
+const captureCorrelationIds = require('@perform/lambda-powertools-middleware-correlation-ids')
+
+const AWS_REGION = process.env.AWS_REGION || process.env.AWS_DEFAULT_REGION
+const FUNCTION_NAME = process.env.AWS_LAMBDA_FUNCTION_NAME
+const FUNCTION_VERSION = process.env.AWS_LAMBDA_FUNCTION_VERSION
+const ENV = process.env.ENVIRONMENT || process.env.STAGE
+
+if (!process.env.DATADOG_PREFIX) {
+  process.env.DATADOG_PREFIX = FUNCTION_NAME + '.'
+}
+
+process.env.DATADOG_TAGS = `awsRegion:${AWS_REGION},functionName:${FUNCTION_NAME},functionVersion:${FUNCTION_VERSION},environment:${ENV}`
+
+module.exports = f => {
+  return middy(f)
+    .use(captureCorrelationIds({ sampleDebugLogRate: 0.01 }))
+    .use(sampleLogging({ sampleRate: 0.01 }))
+}
