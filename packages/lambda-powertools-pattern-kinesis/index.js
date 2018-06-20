c9ed93d47d0a2793baa8bda4b31b02be7cc280b1
Yan Cui <theburningmonk@gmail.com>
2018-06-20 17:44:36 +0200

    - initialize DATADOG_PREFIX if one is not set already - published v0.5.0

--- packages/lambda-powertools-pattern-kinesis/index.js
+++ packages/lambda-powertools-pattern-kinesis/index.js
@@ -7,6 +7,10 @@ const FUNCTION_NAME = process.env.AWS_LAMBDA_FUNCTION_NAME
 const FUNCTION_VERSION = process.env.AWS_LAMBDA_FUNCTION_VERSION
 const ENV = process.env.ENVIRONMENT || process.env.STAGE
 
+if (!process.env.DATADOG_PREFIX) {
+  process.env.DATADOG_PREFIX = FUNCTION_NAME + '.'
+}
+
 process.env.DATADOG_TAGS = `awsRegion:${AWS_REGION},functionName:${FUNCTION_NAME},functionVersion:${FUNCTION_VERSION},environment:${ENV}`
 
 module.exports = f => {
