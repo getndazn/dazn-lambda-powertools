00088c0563bb050934cdf2e561bb876a9a970ffc
Yan Cui <theburningmonk@gmail.com>
2018-08-03 14:33:00 +0100

    - added sqs client

---
+++ packages/lambda-powertools-sqs-client/__tests__/index.js
@@ -0,0 +1,105 @@
+const AWS = require('aws-sdk')
+
+const mockSendMessage = jest.fn()
+const mockSendMessageBatch = jest.fn()
+AWS.SQS.prototype.sendMessage = mockSendMessage
+AWS.SQS.prototype.sendMessageBatch = mockSendMessageBatch
+
+const SQS = require('../index')
+const CorrelationIds = require('@perform/lambda-powertools-correlation-ids')
+
+beforeEach(() => {
+  mockSendMessage.mockReturnValueOnce({
+    promise: async () => Promise.resolve()
+  })
+  mockSendMessageBatch.mockReturnValueOnce({
+    promise: async () => Promise.resolve()
+  })
+})
+
+afterEach(() => {
+  mockSendMessage.mockClear()
+  mockSendMessageBatch.mockClear()
+  CorrelationIds.clearAll()
+})
+
+describe('SendMessage', () => {
+  const verifySendMessage = async (attributes) => {
+    const params = {
+      MessageBody: 'test',
+      QueueUrl: 'queue-url'
+    }
+    await SQS.sendMessage(params).promise()
+  
+    expect(mockSendMessage).toBeCalledWith({
+      MessageBody: 'test',
+      QueueUrl: 'queue-url',
+      MessageAttributes: attributes
+    })
+  }
+
+  test('When there are no correlation IDs, MessageAttributes is empty', async () => {
+    await verifySendMessage({})
+  })
+
+  test('Correlation IDs are forwarded in MessageAttributes', async () => {
+    CorrelationIds.replaceAllWith({
+      'x-correlation-id': 'id',
+      'debug-log-enabled': 'true'
+    })
+
+    await verifySendMessage({
+      'x-correlation-id': {
+        DataType: 'String',
+        StringValue: 'id'
+      },
+      'debug-log-enabled': {
+        DataType: 'String',
+        StringValue: 'true'
+      }
+    })
+  })
+})
+
+describe('SendMessageBatch', () => {
+  const verifySendMessageBatch = async (attributes) => {
+    const params = {
+      Entries: [
+        { Id: '1', MessageBody: 'test-1' },
+        { Id: '2', MessageBody: 'test-2' }
+      ],
+      QueueUrl: 'queue-url'
+    }
+    await SQS.sendMessageBatch(params).promise()
+  
+    expect(mockSendMessageBatch).toBeCalledWith({
+      Entries: [
+        { Id: '1', MessageBody: 'test-1', MessageAttributes: attributes },
+        { Id: '2', MessageBody: 'test-2', MessageAttributes: attributes }
+      ],
+      QueueUrl: 'queue-url'
+    })
+  }
+
+  test('When there are no correlation IDs, MessageAttributes is empty', async () => {  
+    await verifySendMessageBatch({})
+  })
+
+  test('Correlation IDs are forwarded in MessageAttributes', async () => {
+    CorrelationIds.replaceAllWith({
+      'x-correlation-id': 'id',
+      'debug-log-enabled': 'true'
+    })
+
+    await verifySendMessageBatch({
+      'x-correlation-id': {
+        DataType: 'String',
+        StringValue: 'id'
+      },
+      'debug-log-enabled': {
+        DataType: 'String',
+        StringValue: 'true'
+      }
+    })
+  })
+})
---
+++ packages/lambda-powertools-sqs-client/index.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk')
+const client = new AWS.SQS()
+const CorrelationIds = require('@perform/lambda-powertools-correlation-ids')
+
+function addCorrelationIds(messageAttributes) {
+  let attributes = {}
+  let correlationIds = CorrelationIds.get()
+  for (let key in correlationIds) {
+    attributes[key] = {
+      DataType: 'String',
+      StringValue: correlationIds[key]
+    }
+  }
+
+  // use `attribtues` as base so if the user's message attributes would override
+  // our correlation IDs
+  return Object.assign(attributes, messageAttributes || {})
+}
+
+const originalSendMessage = client.sendMessage
+client.sendMessage = function () {
+  const params = arguments[0]
+  const newMessageAttributes = addCorrelationIds(params.MessageAttributes)
+  arguments[0] = Object.assign({}, params, { MessageAttributes: newMessageAttributes })
+
+  return originalSendMessage.apply(this, arguments)
+}
+
+const originalSendMessageBatch = client.sendMessageBatch
+client.sendMessageBatch = function () {
+  const params = arguments[0]
+  const newEntires = params.Entries.map(entry => {
+    const newMessageAttributes = addCorrelationIds(entry.MessageAttributes)
+    return Object.assign({}, entry, { MessageAttributes: newMessageAttributes })
+  })
+  arguments[0] = Object.assign({}, params, { Entries: newEntires })
+
+  return originalSendMessageBatch.apply(this, arguments)
+}
+
+module.exports = client
