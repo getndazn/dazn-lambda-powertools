4fa54853bb53382d376d39aa20117d73aba94ed6
ThomasRogers <thomas.rogers@dazn.com>
2019-02-16 18:16:54 +0100

    Add more tests

--- packages/lambda-powertools-middleware-obfuscater/__tests__/obfuscate.js
+++ packages/lambda-powertools-middleware-obfuscater/__tests__/obfuscate.js
@@ -10,37 +10,63 @@ test('should obfuscate events using the filter', () => {
 })
 
 test('should obfuscate events with arrays using the filter', () => {
-  const event = invokeObfuscater({ test: [ { foo: 'wat' }, { foo: 'bar' } ] }, ['test.*.foo'])
+  const event = invokeObfuscater({ test: [{ foo: 'wat' }, { foo: 'bar' }] }, [
+    'test.*.foo'
+  ])
   expect(event.test[0].foo).toEqual('******')
   expect(event.test[1].foo).toEqual('******')
 })
 
 test('should obfuscate events with multipleArrays arrays using the filter', () => {
-  const event = invokeObfuscater({ test: [ { foo: { bar: [ { baz: 'heyo' } ] } }, { foo: { bar: [ { baz: 'hey' } ] } } ] }, ['test.*.foo.bar.*.baz'])
+  const event = invokeObfuscater(
+    {
+      test: [
+        { foo: { bar: [{ baz: 'heyo' }] } },
+        { foo: { bar: [{ baz: 'hey' }] } }
+      ]
+    },
+    ['test.*.foo.bar.*.baz']
+  )
   expect(event.test[0].foo.bar[0].baz).toEqual('******')
 })
 
 test('should obfuscate every field on an object using the filter', () => {
-  const event = invokeObfuscater({ test: [ { foo: { bar: [ { baz: { bloop: { blah: 'Please Obfuscate Me' } } } ] } }, { foo: { bar: [ { baz: 'hey' } ] } } ] }, ['test.*.foo'])
+  const event = invokeObfuscater(
+    {
+      test: [
+        { foo: { bar: [{ baz: { bloop: { blah: 'Please Obfuscate Me' } } }] } },
+        { foo: { bar: [{ baz: 'hey' }] } }
+      ]
+    },
+    ['test.*.foo']
+  )
   expect(event.test[0].foo.bar[0].baz.bloop.blah).toEqual('******')
 })
 
 test('should not filter fields that dont exist', () => {
   const rawEvent = { test: { foo: { bar: { buzz: 'Nope' } } } }
-  const event = invokeObfuscater({ test: { foo: { bar: { buzz: 'Nope' } } } }, ['test.foo.bar.boop'])
+  const event = invokeObfuscater({ test: { foo: { bar: { buzz: 'Nope' } } } }, [
+    'test.foo.bar.boop'
+  ])
   expect(event).toEqual(rawEvent)
 })
 
 test('should not filter fields that dont exist with arrays', () => {
   const event = { test: { foo: { bar: [{ buzz: 'Nope' }] } } }
-  const obfuscatedEvent = invokeObfuscater({ test: { foo: { bar: [{ buzz: 'Nope' }] } } }, ['test.foo.bar.*.blap'])
+  const obfuscatedEvent = invokeObfuscater(
+    { test: { foo: { bar: [{ buzz: 'Nope' }] } } },
+    ['test.foo.bar.*.blap']
+  )
 
   expect(event).toEqual(obfuscatedEvent)
 })
 
 test('should not filter when nothing passed', () => {
   const event = { test: { foo: { bar: [{ buzz: 'Nope' }] } } }
-  const obfuscatedEvent = invokeObfuscater({ test: { foo: { bar: [{ buzz: 'Nope' }] } } }, [])
+  const obfuscatedEvent = invokeObfuscater(
+    { test: { foo: { bar: [{ buzz: 'Nope' }] } } },
+    []
+  )
 
   expect(event).toEqual(obfuscatedEvent)
 })
@@ -49,4 +75,11 @@ test('should not filter when object doesnt exist', () => {
   const event = { test: { foo: { bar: [{ buzz: 'Nope' }] } } }
   const obfuscatedEvent = invokeObfuscater(event, ['empty'])
   expect(event).toEqual(obfuscatedEvent)
+})
+
+test('should obfuscate objects when it exists in one object in an array, but not others', () => {
+  const event = { test: [{ foo: 'Bar', Baz: 'Boop' }, { foo: 'Bar' }] }
+  const obfuscatedEvent = invokeObfuscater(event, ['test.*.Baz'])
+  expect(obfuscatedEvent.test[0].Baz).toEqual('******')
+  expect(obfuscatedEvent.test[1].Baz).toBeUndefined()
 })
