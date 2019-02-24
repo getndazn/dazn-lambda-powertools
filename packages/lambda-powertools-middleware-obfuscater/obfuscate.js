17058f88796a5de558458a2074025d3288572001
ThomasRogers <thomas.rogers@dazn.com>
2019-02-24 12:21:52 +0100

    refactor: change naming

--- packages/lambda-powertools-middleware-obfuscater/obfuscate.js
+++ packages/lambda-powertools-middleware-obfuscater/obfuscate.js
@@ -2,10 +2,10 @@ const _ = require('lodash/fp')
 
 function convertToObfuscatedEvent (event, fieldsToObfuscate) {
   const obfuscatedObject = _.flow(
-    _.map(getUnobfuscatedObject(event)), // Retrieve the path to the object to obfuscate { a.b.c.obfuscate }
-    _.map(obfuscateObject), // Iterate through everything
-    _.mergeAll,
-    removeEmpties
+    _.map(getUnobfuscatedObject(event)), // Retrieve the path to the object to obfuscate { a.b.c.**** }
+    _.map(obfuscate), // Iterate through everything
+    _.mergeAll, // Merge them all together creating one unified element
+    removeEmptyObjects // If any have returned an empty object, remove them as they are to be ignored
   )(fieldsToObfuscate)
 
   // Deep merge the event and obfuscation together - returning an obfuscated event
@@ -24,20 +24,22 @@ const getUnobfuscatedObject = event => fieldName => {
 
     if (element === '*') {
       const newFieldName = split.slice(index + 1).join('.')
-      const oldFieldName = split.splice(0, index)
+      const oldFieldName = split.slice(0, index).join('.')
+      // we've hit an array, so recurse into this and return { a: { b: [{c: "****"}]}}
       return _.set(oldFieldName, _.map(arrayVal => getUnobfuscatedObject(arrayVal)(newFieldName))(eventPointer), object)
     }
 
     eventPointer = _.get(element)(eventPointer)
-    // If it's not an array we just build up the object more until the last one.
+    // If it's not an array we just build up the object more until the last one where we replace it with the object in the event
     pointer[element] = (index !== split.length - 1 && {}) || eventPointer
     pointer = pointer[element]
   }
 
+  // Return the minimal path to the object { a.b.c.d.obfuscation }
   return object
 }
 
-const obfuscateObject = field => {
+const obfuscate = field => {
   return _.flow(
     _.map(obfuscateChildren(field)),
     _.mergeAll
@@ -47,31 +49,27 @@ const obfuscateObject = field => {
 const obfuscateChildren = field => key => {
   const newField = _.get(key)(field)
 
-  if (newField instanceof Array) {
-    return { [key]: _.map(obfuscateObject)(newField) }
-  }
+  if (!_.isUndefined(newField)) {
+    if (newField instanceof Array) {
+      return { [key]: _.map(obfuscate)(newField) }
+    }
 
-  if (newField instanceof Object) {
-    return { [key]: obfuscateObject(newField) }
-  }
+    if (newField instanceof Object) {
+      return { [key]: obfuscate(newField) }
+    }
 
-  if (newField === undefined) {
-    return undefined
+    return ({ [key]: '******' })
   }
-
-  return ({ [key]: '******' })
 }
 
-const removeEmpties = (object) => {
+const removeEmptyObjects = (object) => {
   return (function remove (obj) {
     for (var key in obj) {
-      if (!obj[key] || typeof obj[key] !== 'object') {
-        continue // If we aren't an object just continue
-      }
-
-      remove(obj[key]) // Recurse down finding the children nodes and removing them if needed
-      if (Object.keys(obj[key]).length === 0) {
-        delete obj[key] // Remove the cloned elements empty child
+      if (obj[key] && typeof obj[key] === 'object') {
+        remove(obj[key]) // Recurse down finding the children nodes and removing them if needed
+        if (_.size(obj[key]) === 0) {
+          delete obj[key] // Remove the cloned elements empty child
+        }
       }
     }
 
