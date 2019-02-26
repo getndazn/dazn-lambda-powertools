f0b3e46f55e62143a54ab2146c3ad8e291fd9c4f
Simon Tabor <me@simontabor.com>
2019-02-26 11:03:42 +0000

    test(logger): refactor to use describe/it

--- packages/lambda-powertools-logger/__tests__/index.js
+++ packages/lambda-powertools-logger/__tests__/index.js
@@ -80,104 +80,122 @@ const enabledAt = (method, enabledLevels) => {
   })
 }
 
-test('Logs are captured as JSON', () => {
-  Log.debug('test')
-  verify(log => expect(log.message).toBe('test'))
-})
+describe('Logger', () => {
+  it('captures logs as JSON', () => {
+    Log.debug('test')
+    verify(log => expect(log.message).toBe('test'))
+  })
 
-test('Debug logs have "level" of "DEBUG"', () => hasRightLevel(Log.debug, 'DEBUG', 20))
-test('Info logs have "level" of "INFO"', () => hasRightLevel(Log.info, 'INFO', 30))
-test('Warn logs have "level" of "WARN"', () => hasRightLevel(Log.warn, 'WARN', 40))
-test('Error logs have "level" of "ERROR"', () => hasRightLevel(Log.error, 'ERROR', 50))
+  describe('Level properties', () => {
+    it('sets "level" of "DEBUG" for debug logs', () => hasRightLevel(Log.debug, 'DEBUG', 20))
+    it('sets "level" of "INFO" for info logs', () => hasRightLevel(Log.info, 'INFO', 30))
+    it('sets "level" of "WARN" for warn logs', () => hasRightLevel(Log.warn, 'WARN', 40))
+    it('sets "level" of "ERROR" for error logs', () => hasRightLevel(Log.error, 'ERROR', 50))
+  })
 
-test('Default contexts (region, funciton, env) are included in debug logs', () => defaultContextsAreIncluded(Log.debug))
-test('Default contexts (region, funciton, env) are included in info logs', () => defaultContextsAreIncluded(Log.debug))
-test('Default contexts (region, funciton, env) are included in warn logs', () => defaultContextsAreIncluded(Log.debug))
-test('Default contexts (region, funciton, env) are included in error logs', () => defaultContextsAreIncluded(Log.debug))
+  describe('Default context (region, function, env)', () => {
+    it('includes them in debug logs', () => defaultContextsAreIncluded(Log.debug))
+    it('includes them in info logs', () => defaultContextsAreIncluded(Log.info))
+    it('includes them in warn logs', () => defaultContextsAreIncluded(Log.warn))
+    it('includes them in error logs', () => defaultContextsAreIncluded(Log.error))
+  })
 
-test('Params are included in debug logs', () => paramsAreIncluded(Log.debug))
-test('Params are included in info logs', () => paramsAreIncluded(Log.info))
-test('Params are included in warn logs', () => paramsAreIncluded(Log.warn))
-test('Params are included in error logs', () => paramsAreIncluded(Log.error))
+  describe('Params', () => {
+    it('includes them in debug logs', () => paramsAreIncluded(Log.debug))
+    it('includes them in info logs', () => paramsAreIncluded(Log.info))
+    it('includes them in warn logs', () => paramsAreIncluded(Log.warn))
+    it('includes them in error logs', () => paramsAreIncluded(Log.error))
 
-test("Params can't override level and message [debug]", () => paramsCannotOverrideLevelAndMessage(Log.debug))
-test("Params can't override level and message [info]", () => paramsCannotOverrideLevelAndMessage(Log.info))
-test("Params can't override level and message [warn]", () => paramsCannotOverrideLevelAndMessage(Log.warn))
-test("Params can't override level and message [error]", () => paramsCannotOverrideLevelAndMessage(Log.error))
+    describe('when params includes level or message', () => {
+      it("doesn't override level and message [debug]", () => paramsCannotOverrideLevelAndMessage(Log.debug))
+      it("doesn't override level and message [info]", () => paramsCannotOverrideLevelAndMessage(Log.info))
+      it("doesn't override level and message [warn]", () => paramsCannotOverrideLevelAndMessage(Log.warn))
+      it("doesn't override level and message [error]", () => paramsCannotOverrideLevelAndMessage(Log.error))
+    })
+  })
 
-test('Error details are included in warn logs', () => errorsAreIncluded(Log.warn))
-test('Error details are included in error logs', () => errorsAreIncluded(Log.error))
+  describe('Error details', () => {
+    it('includes error details in warn logs', () => errorsAreIncluded(Log.warn))
+    it('includes error details in error logs', () => errorsAreIncluded(Log.error))
+  })
 
-test('Debug logs are enabled at DEBUG level', () => enabledAt('debug', [ 'DEBUG' ]))
-test('Info logs are enabled at DEBUG and INFO levels', () => enabledAt('info', [ 'DEBUG', 'INFO' ]))
-test('Warn logs are enabled at DEBUG, INFO and WARN levels', () => enabledAt('warn', [ 'DEBUG', 'INFO', 'WARN' ]))
-test('Error logs are enabled at all levels', () => enabledAt('error', [ 'DEBUG', 'INFO', 'WARN', 'ERROR' ]))
+  describe('Log level', () => {
+    it('enables debug logs at DEBUG level', () => enabledAt('debug', [ 'DEBUG' ]))
+    it('enables info logs at DEBUG and INFO levels', () => enabledAt('info', [ 'DEBUG', 'INFO' ]))
+    it('enables warn logs at at DEBUG, INFO and WARN levels', () => enabledAt('warn', [ 'DEBUG', 'INFO', 'WARN' ]))
+    it('enables error logs at all levels', () => enabledAt('error', [ 'DEBUG', 'INFO', 'WARN', 'ERROR' ]))
+  })
 
-test('enableDebug() temporarily enables logging at DEBUG level', () => {
-  process.env.LOG_LEVEL = 'INFO'
-  const levelLogger = new Log()
+  describe('.enableDebug', () => {
+    it('temporarily enables logging at DEBUG level', () => {
+      process.env.LOG_LEVEL = 'INFO'
+      const levelLogger = new Log()
 
-  const rollback = levelLogger.enableDebug()
+      const rollback = levelLogger.enableDebug()
 
-  levelLogger.debug('this should be logged')
+      levelLogger.debug('this should be logged')
 
-  verify(x => expect(x.message).toBe('this should be logged'))
+      verify(x => expect(x.message).toBe('this should be logged'))
 
-  consoleLog.mockClear()
+      consoleLog.mockClear()
 
-  rollback() // back to INFO logging
+      rollback() // back to INFO logging
 
-  levelLogger.debug('this should not be logged')
+      levelLogger.debug('this should not be logged')
 
-  expect(consoleLog).not.toBeCalled()
-})
+      expect(consoleLog).not.toBeCalled()
+    })
+  })
 
-test('LOG_LEVEL env var is not case sensitive', () => {
-  const shouldBeLogged = (logLevel, log, expectedLevel) => {
-    process.env.LOG_LEVEL = logLevel
-    log('this should be logged')
-    verify(x => {
-      expect(x.message).toBe('this should be logged')
-      expect(x.sLevel).toBe(expectedLevel)
+  describe('process.env.LOG_LEVEL', () => {
+    it('is not case sensitive', () => {
+      const shouldBeLogged = (logLevel, log, expectedLevel) => {
+        process.env.LOG_LEVEL = logLevel
+        log('this should be logged')
+        verify(x => {
+          expect(x.message).toBe('this should be logged')
+          expect(x.sLevel).toBe(expectedLevel)
+        })
+
+        consoleLog.mockClear()
+      }
+
+      shouldBeLogged('debug', Log.debug, 'DEBUG')
+      shouldBeLogged('deBug', Log.debug, 'DEBUG')
+      shouldBeLogged('DeBug', Log.debug, 'DEBUG')
+
+      shouldBeLogged('info', Log.info, 'INFO')
+      shouldBeLogged('Info', Log.info, 'INFO')
+      shouldBeLogged('InfO', Log.info, 'INFO')
+
+      shouldBeLogged('warn', Log.warn, 'WARN')
+      shouldBeLogged('Warn', Log.warn, 'WARN')
+      shouldBeLogged('WarN', Log.warn, 'WARN')
+
+      shouldBeLogged('error', Log.error, 'ERROR')
+      shouldBeLogged('Error', Log.error, 'ERROR')
+      shouldBeLogged('ErroR', Log.error, 'ERROR')
     })
 
-    consoleLog.mockClear()
-  }
+    it('treats misconfigured inputs as DEBUG', () => {
+      const shouldBeLogged = (logLevel, method) => {
+        process.env.LOG_LEVEL = logLevel
+        const levelLogger = new Log()
+        levelLogger[method]('this should be logged')
+        verify(x => expect(x.message).toBe('this should be logged'))
 
-  shouldBeLogged('debug', Log.debug, 'DEBUG')
-  shouldBeLogged('deBug', Log.debug, 'DEBUG')
-  shouldBeLogged('DeBug', Log.debug, 'DEBUG')
+        consoleLog.mockClear()
+      }
 
-  shouldBeLogged('info', Log.info, 'INFO')
-  shouldBeLogged('Info', Log.info, 'INFO')
-  shouldBeLogged('InfO', Log.info, 'INFO')
+      shouldBeLogged('bedug', 'debug')
+      shouldBeLogged('bedug', 'info')
+      shouldBeLogged('bedug', 'warn')
+      shouldBeLogged('bedug', 'error')
 
-  shouldBeLogged('warn', Log.warn, 'WARN')
-  shouldBeLogged('Warn', Log.warn, 'WARN')
-  shouldBeLogged('WarN', Log.warn, 'WARN')
-
-  shouldBeLogged('error', Log.error, 'ERROR')
-  shouldBeLogged('Error', Log.error, 'ERROR')
-  shouldBeLogged('ErroR', Log.error, 'ERROR')
-})
-
-test('misconfigured LOG_LEVEL env var is ignored and treated at DEBUG', () => {
-  const shouldBeLogged = (logLevel, method) => {
-    process.env.LOG_LEVEL = logLevel
-    const levelLogger = new Log()
-    levelLogger[method]('this should be logged')
-    verify(x => expect(x.message).toBe('this should be logged'))
-
-    consoleLog.mockClear()
-  }
-
-  shouldBeLogged('bedug', 'debug')
-  shouldBeLogged('bedug', 'info')
-  shouldBeLogged('bedug', 'warn')
-  shouldBeLogged('bedug', 'error')
-
-  shouldBeLogged('inf0', 'debug')
-  shouldBeLogged('inf0', 'info')
-  shouldBeLogged('inf0', 'warn')
-  shouldBeLogged('inf0', 'error')
+      shouldBeLogged('inf0', 'debug')
+      shouldBeLogged('inf0', 'info')
+      shouldBeLogged('inf0', 'warn')
+      shouldBeLogged('inf0', 'error')
+    })
+  })
 })
