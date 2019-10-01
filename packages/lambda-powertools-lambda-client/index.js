71270448524aeab2a8701ff43e92415ba07312c6
theburningmonk <theburningmonk@gmail.com>
2019-10-02 00:09:44 +0100

    fix: removed HTTP keep-alive code
    Closes #89

--- packages/lambda-powertools-lambda-client/index.js
+++ packages/lambda-powertools-lambda-client/index.js
@@ -1,18 +1,4 @@
 const AWS = require('aws-sdk')
-const https = require('https')
-const sslAgent = new https.Agent({
-  keepAlive: true,
-  maxSockets: 50,
-  rejectUnauthorized: true
-})
-sslAgent.setMaxListeners(0)
-
-AWS.config.update({
-  httpOptions: {
-    agent: sslAgent
-  }
-})
-
 const client = new AWS.Lambda()
 const Log = require('@dazn/lambda-powertools-logger')
 const CorrelationIds = require('@dazn/lambda-powertools-correlation-ids')
