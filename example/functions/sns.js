6d2fb61cd905ccd0bc11a43b07b181a3ee392edf
theburningmonk <theburningmonk@gmail.com>
2019-10-02 20:29:42 +0100

    chore: updated example to include dynamodb streams

--- example/functions/sns.js
+++ example/functions/sns.js
@@ -3,6 +3,7 @@ const Lambda = require('@dazn/lambda-powertools-lambda-client')
 const SFN = require('@dazn/lambda-powertools-step-functions-client')
 const CorrelationIds = require('@dazn/lambda-powertools-correlation-ids')
 const snsProcessor = require('@dazn/lambda-powertools-pattern-basic')
+const uuid = require('uuid/v4')
 
 module.exports.handler = snsProcessor(async (event, context) => {
   CorrelationIds.set('event-source', 'sns')
@@ -21,7 +22,7 @@ module.exports.handler = snsProcessor(async (event, context) => {
   const startReq = {
     stateMachineArn: stateMachineArn,
     input: JSON.stringify({ x: 13, y: 29 }),
-    name: 'test'
+    name: uuid()
   }
 
   Log.debug('starting a SFN execution', { stateMachineArn: stateMachineArn })
