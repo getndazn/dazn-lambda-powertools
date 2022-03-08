1a37a2af8358e37673f9985e946159ad7525bdc3
sumit-ba <sumit.agarwal@buyerassist.io>
2022-03-08 12:57:13 +0530

    feat(sns-client): add support for batch publish.

--- packages/lambda-powertools-sns-client/index.d.ts
+++ packages/lambda-powertools-sns-client/index.d.ts
@@ -1,7 +1,7 @@
 import awsSNS from "aws-sdk/clients/sns";
 import { AWSError } from "aws-sdk/lib/error";
 import { Request } from "aws-sdk/lib/request";
-import CorrelationIds from "@dazn/lambda-powertools-correlation-ids";
+import CorrelationIds from "@buyerassist/dazn-lambda-powertools-correlation-ids";
 
 declare const SNS: awsSNS & {
   publishWithCorrelationIds(
@@ -9,5 +9,10 @@ declare const SNS: awsSNS & {
     params: awsSNS.Types.PublishInput,
     callback?: (err: AWSError, data: awsSNS.Types.PublishResponse) => void
   ): Request<awsSNS.Types.PublishResponse, AWSError>;
-};
+  publishBatchWithCorrelationIds(
+    correlationId: CorrelationIds,
+    params: awsSNS.Types.PublishBatchInput,
+    callback?: (err: AWSError, data: awsSNS.Types.PublishBatchResponse) => void
+  ): Request<awsSNS.Types.PublishBatchResponse, AWSError>;
+}
 export default SNS;
